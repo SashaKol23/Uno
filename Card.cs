309b2043ead3e11f2746f3f8fd9bfbf33c771da8
Aleksandr Kolotnecha <89186608184@mail.ru>
2022-01-08 15:50:22 +0300

    первый день

---
+++ Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno
+{
+    public class Card
+    {
+        public int namber;
+
+        public int colour;
+        enum colors
+        {
+            Red,
+            Yellow,
+            Green,
+            Blue,
+            Black
+        }
+        public int abiliti;
+        enum abiliti
+        {
+            TakeTwo,
+            TakeFour,
+            Apass,
+            Uturn,
+            ColorSelection
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Uno
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
